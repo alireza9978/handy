44aeefa82025f676c0e5e124a2c8075d090a9a68
Alireza Rahimi <rahimialireza.az@gmail.com>
2021-09-28 17:37:08 +0330

    add video and camera input

--- app/src/main/java/ir/coleo/handy/activities/MainActivity.java
+++ app/src/main/java/ir/coleo/handy/activities/MainActivity.java
@@ -2,6 +2,7 @@ package ir.coleo.handy.activities;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -10,6 +11,8 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -29,25 +32,67 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MAIN";
     private ActivityResultLauncher<Intent> firstImageGetter;
     private ActivityResultLauncher<Intent> secondImageGetter;
+    private ActivityResultLauncher<Intent> videoGetter;
+
     private Bitmap firstImage;
     private Bitmap secondImage;
+    private Uri videoUri;
+
+    private Button loadFirstImageButton;
+    private Button loadSecondImageButton;
     private ImageView firstImageView;
     private ImageView secondImageView;
+
+    private Button loadVideoButton;
+    private VideoView videoView;
+
+    private Button startProcessButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setInputSourceDropDownValue();
+        defineProcessViews();
         defineImageViews();
+        defineVideoViews();
+        setInputSourceDropDownValue();
         setUsageDropDownValue();
         setAngleDropDownValue();
         showImageInputState();
     }
 
-    private void defineImageViews(){
+    private void defineProcessViews() {
+        startProcessButton = findViewById(R.id.start_process_button);
+    }
+
+    private void defineVideoViews() {
+        loadVideoButton = findViewById(R.id.button_load_video);
+        videoView = findViewById(R.id.video_view);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        // The Intent to access gallery and read a video file.
+        videoGetter = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result -> {
+                    Intent resultIntent = result.getData();
+                    if (resultIntent != null) {
+                        if (result.getResultCode() == RESULT_OK) {
+                            videoUri = resultIntent.getData();
+                            videoView.setMediaController(mediaController);
+                            videoView.setVideoURI(videoUri);
+                            videoView.requestFocus();
+                            videoView.start();
+                            checkVideoAndShowProcess();
+                        }
+                    }
+                });
+
+    }
+
+    private void defineImageViews() {
         firstImageView = findViewById(R.id.first_image_imageview);
         secondImageView = findViewById(R.id.second_image_imageview);
+        loadFirstImageButton = findViewById(R.id.first_button_load_picture);
+        loadSecondImageButton = findViewById(R.id.second_button_load_picture);
         // The Intent to access gallery and read images as bitmap.
         firstImageGetter = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -58,6 +103,7 @@ public class MainActivity extends AppCompatActivity {
                             try {
                                 firstImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultIntent.getData());
                                 firstImageView.setImageBitmap(firstImage);
+                                checkImagesAndShowProcess();
                             } catch (IOException e) {
                                 Log.e(TAG, "Bitmap reading error:" + e);
                             }
@@ -74,12 +120,29 @@ public class MainActivity extends AppCompatActivity {
                             try {
                                 secondImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultIntent.getData());
                                 secondImageView.setImageBitmap(secondImage);
+                                checkImagesAndShowProcess();
                             } catch (IOException e) {
                                 Log.e(TAG, "Bitmap reading error:" + e);
                             }
                         }
                     }
                 });
+    }
+
+    private void checkImagesAndShowProcess() {
+        if (firstImage != null && secondImage != null) {
+            showProcessButton();
+        } else {
+            hideProcessButton();
+        }
+    }
+
+    private void checkVideoAndShowProcess() {
+        if (videoUri != null) {
+            showProcessButton();
+        } else {
+            hideProcessButton();
+        }
     }
 
     private void setInputSourceDropDownValue() {
@@ -91,13 +154,19 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 if (id == 0) {
+                    hideProcessButton();
+                    hideVideoInputState();
                     showImageInputState();
                 }
                 if (id == 1) {
+                    hideProcessButton();
+                    hideImageInputState();
                     showVideoInputState();
                 }
                 if (id == 2) {
-                    showCameraInputState();
+                    hideImageInputState();
+                    hideVideoInputState();
+                    showProcessButton();
                 }
             }
 
@@ -107,7 +176,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 
     private void setAngleDropDownValue() {
         AppCompatSpinner dropdown = findViewById(R.id.angle_spinner);
@@ -123,11 +191,32 @@ public class MainActivity extends AppCompatActivity {
         dropdown.setAdapter(adapter);
     }
 
+
+    private void hideProcessButton() {
+        startProcessButton.setVisibility(View.GONE);
+    }
+
+    private void hideImageInputState() {
+        loadFirstImageButton.setVisibility(View.GONE);
+        loadSecondImageButton.setVisibility(View.GONE);
+        firstImageView.setVisibility(View.GONE);
+        secondImageView.setVisibility(View.GONE);
+
+        firstImage = null;
+        secondImage = null;
+
+        firstImageView.setImageBitmap(null);
+        secondImageView.setImageBitmap(null);
+    }
+
+    private void hideVideoInputState() {
+        loadVideoButton.setVisibility(View.GONE);
+        videoView.setVisibility(View.GONE);
+        videoView.setVideoURI(null);
+        videoView.stopPlayback();
+    }
+
     private void showImageInputState() {
-
-        Button loadFirstImageButton = findViewById(R.id.first_button_load_picture);
-        Button loadSecondImageButton = findViewById(R.id.second_button_load_picture);
-
         loadFirstImageButton.setVisibility(View.VISIBLE);
         loadSecondImageButton.setVisibility(View.VISIBLE);
         firstImageView.setVisibility(View.VISIBLE);
@@ -143,14 +232,19 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
     private void showVideoInputState() {
+        loadVideoButton.setVisibility(View.VISIBLE);
+        videoView.setVisibility(View.VISIBLE);
 
+        loadVideoButton.setOnClickListener(
+                v -> {
+                    Intent gallery = new Intent(Intent.ACTION_PICK,
+                            MediaStore.Video.Media.INTERNAL_CONTENT_URI);
+                    videoGetter.launch(gallery);
+                });
     }
 
-
-    private void showCameraInputState() {
-
+    private void showProcessButton() {
+        startProcessButton.setVisibility(View.VISIBLE);
     }
-
 }
